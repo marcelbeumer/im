de37678fe8ae50b86c04c1e69057682b12fe223d
Marcel Beumer <marcel@marcelbeumer.com>
2011-02-14 22:00:51 +0100

    modules, helpers.

--- plugins/im.template.js
+++ plugins/im.template.js
@@ -6,18 +6,28 @@ Supports:
     * Executing code with {%:alert('x')%}
     * Includes
     * Extending ala Django templates
-    * Custom tags
+    * Custom modules
     * Overriding anything... :)
 
 Todo:
+    * find a way to deal with module loading (<% load %> ala Django?)
+    * be able to set default modules
+    * find a way to deal with template library loading and such...
     * more debugging facilities
 --------------------------------------------------------------------------- */
 (function(ns){
     
+    var callee = arguments.callee;
+    
     /* ---------------------------------------------------------------------------
     
     --------------------------------------------------------------------------- */
-    ns.template = function(template, options) {
+    ns.template = {};
+    
+    /* ---------------------------------------------------------------------------
+    
+    --------------------------------------------------------------------------- */
+    ns.template.create = function(template, options) {
         /* ---------------------------------------------------------------------------
         
         --------------------------------------------------------------------------- */
@@ -33,13 +43,14 @@ Todo:
         var _matcher;
         var _processor;
         var _templates;
-        var _tags;
+        var _modules;
+        var _helpers;
         var _initialized;
         
         /* ---------------------------------------------------------------------------
         
         --------------------------------------------------------------------------- */
-        var init = function(templates, tags) {
+        var init = function(templates, modules) {
             if (_initialized) return;
             
             // reset all variables
@@ -48,12 +59,14 @@ Todo:
             // merge objects
             im.extend(_templates, ns.template.templates);
             im.extend(_templates, templates || {});
-            im.extend(_tags, ns.template.tags);
-            im.extend(_tags, tags || {});
+            im.extend(_modules, ns.template.modules);
+            im.extend(_modules, modules || {});
             
             // initialize matcher and processor
-            for (var name in _tags) {
-                _tags[name](_matcher, _processor);
+            var defaults = ns.template.defaults.modules;
+            for (var x = 0; x < defaults.length; x++) {
+                var mod = _modules[defaults[x]];
+                if (mod) mod(_matcher, _processor, _helpers);
             }
             
             _initialized = true;
@@ -69,18 +82,10 @@ Todo:
         /* ---------------------------------------------------------------------------
         
         --------------------------------------------------------------------------- */
-        self.loadParsed = function(code) {
-            _code = code;
-            _fn = new Function("obj", "blocks", _code);
-            return self;
-        };
-        
-        /* ---------------------------------------------------------------------------
-        
-        --------------------------------------------------------------------------- */
         self.reset = function() {
             self.clean();
             _initialized = false;
+            _helpers = {};
             _code = undefined;
             _fn = undefined;
         };
@@ -90,7 +95,7 @@ Todo:
         --------------------------------------------------------------------------- */
         self.clean = function() {
             _templates = {};
-            _tags = {};
+            _modules = {};
             _matcher = {};
             _processor = {};
         };
@@ -98,10 +103,10 @@ Todo:
         /* ---------------------------------------------------------------------------
         
         --------------------------------------------------------------------------- */
-        self.parse = function(templates, tags) {
+        self.parse = function(templates, modules) {
             if (_options.reparse === true) self.reset();
             if (_fn) return self;
-            init(templates, tags);
+            init(templates, modules);
             
             // template
             var t = _template + '';
@@ -130,30 +135,32 @@ Todo:
                 
                 var match = _matcher[c];
                 if (match) { // first try to find a match on this chunk
-                    var m = match(c, mode, code, extcode, _templates, _tags, _options);
+                    var m = match(c, mode, code, extcode, _templates, _modules, _options);
                     if (m) mode = m; // and set the new mode if we got any
                 } else { // if no match then we should process the chunk
                     var proc = _processor[mode];
-                    if (proc) proc(c, mode, code, extcode, _templates, _tags, _options);
+                    if (proc) proc(c, mode, code, extcode, _templates, _modules, _options);
                 }
             }
             
             // create function code
             _code = "\
-            var __o = [];var __extend = null;var __b = {};var __eb = blocks || {};\
+            var __helpers = helpers || {}; var __o = [];var __extend = null;var __b = {};var __eb = blocks || {};\
             " + extcode.join('') + "\
-            with (obj) { \
-                " + code.join('') + "\
+            with(__helpers){\
+                with (obj) { \
+                    " + code.join('') + "\
+                }\
             }\
             if (__extend) {\
-                return __extend(obj, __b);\
+                return __extend(obj, __b, __helpers);\
             } else {\
                 return __o.join('');\
             }";
             
             // create the render fucntion
             try {
-                _fn = new Function("obj", "blocks", _code);
+                _fn = new Function("obj", "blocks", "helpers", _code);
             } catch (e) {
                 throw new Error('template.js: could not parse template.\n' + 
                     '[DEBUG] generated code:\n' + _code + '\n' +
@@ -169,9 +176,9 @@ Todo:
         /* ---------------------------------------------------------------------------
         
         --------------------------------------------------------------------------- */
-        self.render = function(data, templates, tags) {
-            self.parse(templates, tags);
-            return _fn(data);
+        self.render = function(data, templates, modules) {
+            self.parse(templates, modules);
+            return _fn(data, null, _helpers);
         };
         
         /* ---------------------------------------------------------------------------
@@ -183,17 +190,23 @@ Todo:
     /* ---------------------------------------------------------------------------
     
     --------------------------------------------------------------------------- */
-    ns.template.templates = {};
+    ns.template.env = function(ns) {
+        var o = {};
+        callee(o);
+        return o.template;
+    };
     
     /* ---------------------------------------------------------------------------
     
     --------------------------------------------------------------------------- */
-    ns.template.tags = {};
+    ns.template.defaults = {modules : []};
+    ns.template.templates = {};
+    ns.template.modules = {};
     
     /* ---------------------------------------------------------------------------
     text tag
     --------------------------------------------------------------------------- */
-    ns.template.tags.text = function(matcher, processor) {
+    ns.template.modules.text = function(matcher, processor, helpers) {
         
         // escaping / quote code taken from Douglas Crockfords json2.js
         // https://github.com/douglascrockford/JSON-js/blob/master/json2.js
@@ -214,88 +227,112 @@ Todo:
             }) + '"' : '"' + string + '"';
         };
         
-        matcher['%>'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        /* ---------------------------------------------------------------------------
+        
+        --------------------------------------------------------------------------- */
+        helpers.$trim = im.trim;
+        helpers.$isString = im.isString;
+        
+        /* ---------------------------------------------------------------------------
+        
+        --------------------------------------------------------------------------- */
+        matcher['%>'] = function(chunk, mode, code, extcode, templates, modules, options) {
             return 'text';
         };
         
-        processor['text'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        /* ---------------------------------------------------------------------------
+        
+        --------------------------------------------------------------------------- */
+        processor['text'] = function(chunk, mode, code, extcode, templates, modules, options) {
             code.push('/* text */ __o.push(' + quote(chunk) + ');');
         };
     };
     
+    // add as default module
+    ns.template.defaults.modules.push('text');
+    
     /* ---------------------------------------------------------------------------
     value tag
     --------------------------------------------------------------------------- */
-    ns.template.tags.value = function(matcher, processor) {
+    ns.template.modules.value = function(matcher, processor, helpers) {
         
-        matcher['<%='] = function(chunk, mode, code, extcode, templates, tags, options) {
+        matcher['<%='] = function(chunk, mode, code, extcode, templates, modules, options) {
             return 'value';
         };
         
-        processor['value'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        processor['value'] = function(chunk, mode, code, extcode, templates, modules, options) {
             code.push('/* value */ __o.push(' + chunk + ');');
         };
     };
     
+    // add as default module
+    ns.template.defaults.modules.push('value');
+    
     /* ---------------------------------------------------------------------------
     code tag
     --------------------------------------------------------------------------- */
-    ns.template.tags.code = function(matcher, processor) {
+    ns.template.modules.code = function(matcher, processor, helpers) {
         
-        matcher['<%:'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        matcher['<%:'] = function(chunk, mode, code, extcode, templates, modules, options) {
             return 'code';
         };
         
-        processor['code'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        processor['code'] = function(chunk, mode, code, extcode, templates, modules, options) {
             code.push('/* code */' + chunk);
         };
     };
+    
+    // add as default module
+    ns.template.defaults.modules.push('code');
 
     /* ---------------------------------------------------------------------------
     block tag
     --------------------------------------------------------------------------- */
-    ns.template.tags.block = function(matcher, processor) {
+    ns.template.modules.block = function(matcher, processor, helpers) {
         var blockc = 0;
         var blocks = [];
         
-        matcher['<%block'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        matcher['<%block'] = function(chunk, mode, code, extcode, templates, modules, options) {
             blockc++;
             return 'block';
         };
 
-        matcher['<%endblock'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        matcher['<%endblock'] = function(chunk, mode, code, extcode, templates, modules, options) {
             return 'endblock';
         };
         
-        processor['block'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        processor['block'] = function(chunk, mode, code, extcode, templates, modules, options) {
             var name = blocks[blockc] = im.trim(chunk);
             code.push('/* block */ if (__eb[\'' + name + '\']) __o.push(__eb[\'' + name + '\']);');
             code.push('if (!__eb[\'' + name + '\']) __b[\'' + name + '\'] = (function(){ var __o = [];');
         };
 
-        processor['endblock'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        processor['endblock'] = function(chunk, mode, code, extcode, templates, modules, options) {
             var name = blocks[blockc];
             code.push('return __o.join(\'\');})(); __o.push(__b[\'' + name +'\']); /* endblock */');
         };
     };
+    
+    // add as default module
+    ns.template.defaults.modules.push('block');
 
     /* ---------------------------------------------------------------------------
     include tag
     --------------------------------------------------------------------------- */
-    ns.template.tags.include = function(matcher, processor) {
+    ns.template.modules.include = function(matcher, processor, helpers) {
         
-        matcher['<%include'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        matcher['<%include'] = function(chunk, mode, code, extcode, templates, modules, options) {
             return 'include';
         };
         
-        processor['include'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        processor['include'] = function(chunk, mode, code, extcode, templates, modules, options) {
             var name = im.trim(chunk);
             var ft = templates[name]; // foreign template
             if (!ft) return;
             
             try {
                 var fcode = '/* include */ var __' + name + '__include = function(obj, blocks){' +
-                    ft.parse(templates, tags).getParsed() + '};';
+                    ft.parse(templates, modules).getParsed() + '};';
             } catch (e) {
                 throw new Error('im.template: could not include ' + name + 
                     ' because of parse error.\n[DEBUG] original message: ' + e.message);
@@ -305,24 +342,27 @@ Todo:
             code.push('__o.push(__' + name + '__include(obj, blocks));');
         };
     };
+    
+    // add as default module
+    ns.template.defaults.modules.push('include');
 
     /* ---------------------------------------------------------------------------
     include tag
     --------------------------------------------------------------------------- */
-    ns.template.tags.extend = function(matcher, processor) {
+    ns.template.modules.extend = function(matcher, processor, helpers) {
         
-        matcher['<%extend'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        matcher['<%extend'] = function(chunk, mode, code, extcode, templates, modules, options) {
             return 'extend';
         };
         
-        processor['extend'] = function(chunk, mode, code, extcode, templates, tags, options) {
+        processor['extend'] = function(chunk, mode, code, extcode, templates, modules, options) {
             var name = im.trim(chunk);
             var ft = templates[name]; // foreign template
             if (!ft) return;
             
             try {
                 var fcode = '/* extend ' + name + ' */ var __extend = function(obj, blocks){' + 
-                    ft.parse(templates, tags).getParsed() + '};';
+                    ft.parse(templates, modules).getParsed() + '};';
             } catch (e) {
                 throw new Error('im.template: could not extend ' + name + 
                     ' because of parse error.\n[DEBUG] original message: ' + e.message);
@@ -331,5 +371,8 @@ Todo:
             extcode.push(fcode);
         };
     };
+    
+    // add as default module
+    ns.template.defaults.modules.push('extend');
     
 })(window.im || window);
